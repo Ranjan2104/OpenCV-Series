623426960deabaf8f5ebc521522be4f0d0f46016
Amresh2104 <60054130+Ranjan2104@users.noreply.github.com>
2021-05-08 10:49:20 +0530

    initial

---
+++ Mouse Call back or Mouse Click Event.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+windowName = 'Drawing'
+img = np.zeros([512,512,3], np.uint8)
+cv2.namedWindow(windowName)
+
+# Mouse Callback Function
+def draw_circle(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        cv2.circle(img, (x,y), 40, (0,255,0), -1)
+
+    if event == cv2.EVENT_MBUTTONDOWN:
+        cv2.circle(img, (x,y), 20, (0,0,225),-1)
+
+    if event == cv2.EVENT_RBUTTONDOWN:
+        cv2.circle(img, (x, y), 30, (255, 0, 0), -1)
+
+cv2.setMouseCallback(windowName, draw_circle)
+
+while True:
+    cv2.imshow(windowName, img)
+    if cv2.waitKey(1) == 13:
+        break
+cv2.destroyAllWindows()
